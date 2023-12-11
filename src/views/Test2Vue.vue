0a078fbe72c70a1611e20e735c1f5186d9c41504
Vera Kang <sk70885@hotmail.com>
2023-12-12 01:10:19 +0800

    fixed test2

--- src/views/Test2Vue.vue
+++ src/views/Test2Vue.vue
@@ -3,7 +3,8 @@
 function f1() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve('f1 finished');
+      console.log('f1 finished')
+      resolve(30);
     }, 1000);
   });
 }
@@ -11,36 +12,28 @@ function f1() {
 function f2() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve('f2 finished');
-    }, 1000);
+      console.log('f2 finished')
+      resolve(50);
+    }, 2000);
   });
 }
 function f3() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve('f3 finished');
-    }, 1000);
+      console.log('f3 finished')
+      resolve(20);
+    }, 3000);
   });
 }
 
-async function goStart() {
-  const result1 = await f1()
-  console.log(result1);
-  const result2 = await f2()
-  console.log(result2);
-  const result3 = await f3()
-  console.log(result3);
-}
-
-function runMain() {
-  Promise.all([f1(), f2(), f3()])
-    .then(results => {
-      console.log(results)
-      console.log('All async functions have completed successfully !')
-    })
-    .catch(error => {
+async function runMain() {
+  try {
+    const results = await Promise.all([f1(), f2(), f3()])
+    console.log(results)
+    console.log('All async functions have completed successfully !')
+  } catch(error) {
       console.error(error);
-    });
+    };
     
 }
 
@@ -49,8 +42,7 @@ function runMain() {
 <template>
   <h1 class="text-center my-8">Promise.all</h1>
   <div class="text-center">
-    <v-btn @click="goStart" class="mx-2">Go Start!</v-btn>
     <v-btn @click="runMain" class="mx-2">run Main!</v-btn>
   </div>
-  
+
 </template>
